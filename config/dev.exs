a68f18ed7dbe2100d481a2e323e6b4bc40ddfc9f
Antoine Seignard <aseignard@fundimmo.com>
2022-07-29 11:22:31 +0200

    add ssl on phx test 3

--- config/dev.exs
+++ config/dev.exs
@@ -27,7 +27,7 @@ config :task_tester, TaskTesterWeb.Endpoint,
   watchers: [
     # Start the esbuild watcher by calling Esbuild.install_and_run(:default, args)
     esbuild: {Esbuild, :install_and_run, [:default, ~w(--sourcemap=inline --watch)]}
-  ]
+  ],
 
 # ## SSL Support
 #
