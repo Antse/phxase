7cc33b088906ea8b3cf4e552a0eeecfe1a437330
Antoine Seignard <aseignard@fundimmo.com>
2022-07-29 14:11:29 +0200

    update db url in dev config

--- config/dev.exs
+++ config/dev.exs
@@ -4,7 +4,7 @@ import Config
 config :task_tester, TaskTester.Repo,
   username: "postgres",
   password: "postgres",
-  hostname: "localhost",
+  hostname: System.get_env("PGHOST"),
   database: "fortress_dev",
   stacktrace: true,
   show_sensitive_data_on_connection_error: true,
